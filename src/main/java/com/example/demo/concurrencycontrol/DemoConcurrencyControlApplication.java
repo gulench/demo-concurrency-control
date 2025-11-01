be21993f143856cd50b77f44414dd5756446043a
Gulen Chongtham <gulench@gmail.com>
2025-11-01 18:45:09 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/concurrencycontrol/DemoConcurrencyControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.concurrencycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConcurrencyControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConcurrencyControlApplication.class, args);
+	}
+
+}
